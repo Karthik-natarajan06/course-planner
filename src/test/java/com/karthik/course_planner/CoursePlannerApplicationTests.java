379b147f64ac2049465224268d84539df54dedd9
Karthik-natarajan06 <karthik6.natarajan@gmail.com>
2026-06-24 12:41:11 +0200

    Initial Spring Boot project setup with H2

---
+++ src/test/java/com/karthik/course_planner/CoursePlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karthik.course_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursePlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
